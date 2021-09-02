fcef1ccd2b0c8de2f2a88e099bc45919e9d36e42
Prasadika-Shinde <89804406+Prasadika-Shinde@users.noreply.github.com>
2021-09-02 21:49:49 +0530

    Create AscciiNo.c

---
+++ Programs/AscciiNo.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+   
+    char cChar='\0';
+
+    printf("Enter Any character:");
+    scanf("%c",&cChar);
+
+    printf("Ascii values of %c = %d",cChar,cChar);  
+  
+    return 0;
+}
